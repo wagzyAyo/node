6da5ec930c3b6d052854777b3eff8e2d2418fdce
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-04 15:32:47 +0100

    write file

--- Read-write/main.js
+++ Read-write/main.js
@@ -2,5 +2,8 @@ const fs = require('fs');
 const { encode } = require('punycode');
 const readline = require('readline');
 
-let textin = fs.readFileSync('./files/info.txt', 'utf-8');
+let textin = fs.readFileSync('./files/info.txt','utf-8');
 console.log(textin)
+
+let content = `Data read from input file: ${textin}. \n date created ${new Date()}`
+fs.writeFileSync('./files/output.txt', content);
