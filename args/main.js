7611340ca7289e8f16ebb36ff97dea5a5ad4cfe6
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-05 07:27:09 +0100

    args

---
+++ args/main.js
@@ -0,0 +1,9 @@
+const command = process.argv[2]
+
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove'){
+    console.log('Removing note!')
+} else {
+    console.log("Use 'add' to add note, 'remove' to remove note")
+}
