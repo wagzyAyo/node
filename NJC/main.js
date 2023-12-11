c37d3dc8eace55a7607f611b78a805fe848e83de
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-11 20:33:57 +0100

    Global

--- NJC/main.js
+++ NJC/main.js
@@ -6,4 +6,11 @@ if (amount < 10){
     console.log('large number')
 }
 
-console.log(`Hey its my first node app!!`)
+console.log(`Hey its my first node app!!!`)
+
+//GLOBAL - NO WINDOW
+//__dirname - path to current directory
+// __filename - file name
+//require - function to use modules (CommonJs)
+// module - info about current module (file)
+// process - info about env where the program is beign excuted
