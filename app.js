9d5599598bf0c866f7d02ca157dd5beca468609d
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-18 15:02:54 +0100

    app

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-const readline = require('readline');
-const r1 = readline.createInterface(
-    {
-        input: process.stdin,
-        output: process.stdout
-    }
-)
-
-r1.question('please enter your name: ', (name) => {
-    console.log('Welcome to node '+ name )
-    r1.close()
-})
-
-r1.on('close', () => {
-    console.log('Interface closed')
-    process.exit(0)
-})
+console.log('Hello world')
