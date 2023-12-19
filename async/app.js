fb5c6434bd868c30797dff393dbb85e75c259527
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-19 14:49:55 +0100

    promise

---
+++ async/app.js
@@ -0,0 +1,16 @@
+const inventory = {
+    sunglasses: 1900,
+    pants: 1088,
+    bags: 1344
+  };
+  
+  // Write your code below:
+  const myExecutor = (resolve, reject) =>
+  {
+    if (inventory.sunglasses > 0){
+      resolve('Sunglasses order processed.')
+    } else {
+      reject('That item is sold out.')
+    }
+  }
+  
