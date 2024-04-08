320ebc9eddb0f136390fc6d139e3464eab5195ed
Ayo <talktojmcvibes@gmail.com>
2024-04-08 15:30:05 +0100

    Add public css

--- todolistV1/app.js
+++ todolistV1/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 let tasks = []
 
