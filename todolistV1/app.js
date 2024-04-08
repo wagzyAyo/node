62d8f4c51f8a5e98c8fc23cd05ebd2ce3c49e5a6
Ayo <talktojmcvibes@gmail.com>
2024-04-08 11:42:16 +0100

    EJS templating

--- todolistV1/app.js
+++ todolistV1/app.js
@@ -4,14 +4,42 @@ const parser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs')
 
-const day = new Date()
-const today = day.getDay()
-let weekDay = ''
+const day = new Date();
+const today = day.getDay();
+let weekDay = '';
 
-if (today === 0 || today === 6){
-    weekDay = 'Weekend'
-}else {
-    weekDay = 'Week day'
+
+// if (today === 0 || today === 6){
+//     weekDay = 'Weekend'
+// }else {
+//     weekDay = 'Week day'
+// }
+
+switch (today) {
+    case 0:
+        weekDay = 'Sunday'
+        break;
+    case 1:
+        weekDay = 'Monday';
+        break;
+    case 2:
+        weekDay = 'Tuesday';
+        break;
+    case 3:
+        weekDay = 'Wednessday';
+        break;
+    case 4:
+        weekDay = 'Thursday';
+        break;
+    case 5:
+        weekDay = 'Friday';
+        break;
+    case 6:
+        weekDay = 'Saturday'
+
+    default:
+        console.log('Day Not recognized!.')
+        break;
 }
 
 
