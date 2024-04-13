e77619a2af8e97cbc950020b96b733a7cb9983f7
Ayo <talktojmcvibes@gmail.com>
2024-04-13 20:49:46 +0100

    TDLV2

---
+++ todolistV2/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+let tasks = []
+
+const day = new Date();
+
+const options = {
+    weekDay: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+}
+
+const today = day.toLocaleDateString('en-US', options)
+console.log(today)
+
+// if (today === 0 || today === 6){
+//     weekDay = 'Weekend'
+// }else {
+//     weekDay = 'Week day'
+// }
+
+
+
+
+app.get('/', (req, res)=> {
+    res.render('index', {dayOfTheWeek: today, taskItems: tasks})
+})
+
+app.post('/', (req, res)=> {
+    //Get The value of task from index.ejs through post request
+    //and push to the tasks array
+    //then redirect to the home route
+
+    const newTask = req.body.task;
+    tasks.push(newTask)
+
+    res.redirect('/');
+})
+
+app.listen(3000, ()=> {
+    console.log('App listening on port 3000')
+})
